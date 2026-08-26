2a18307945657dc9b98a9e115842544be6909333
Saad Mohamed Rahman <saadmohamedrahman@gmail.com>
2026-08-26 18:04:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const http = require('http');
-const server = http.createServer((req, res) => 
-  {res.writeHead(200, ( 'Content-Type': 'text/plain'  });
-   res.end('Hello from Node.js on Google Cloud!\n');
-));
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello from Node.js on Google Cloud!\n');
+});
+
 const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => {
-  console.log('Server listening on port ${PORT} ');
-});  
+    console.log(`Server listening on port ${PORT}`);
+});
 
 
